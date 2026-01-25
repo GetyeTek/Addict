1a448fb7d72d85e59fd4bb854256888ccffd7821
GetyeTek <getyeteklu2@gmail.com>
2026-01-25 23:16:59 +0300

    Conduit: 1 ops

--- app/src/main/java/com/example/dnsguard/WatcherService.kt
+++ app/src/main/java/com/example/dnsguard/WatcherService.kt
@@ -71,7 +71,8 @@ class WatcherService : Service() {
                     val topPkg = LockManager.currentActivePackage
                     val blockType = LockManager.getActiveBlockType(applicationContext, topPkg)
                     
-                    if (blockType != null && !LockManager.isBootGraceActive() && pm.isInteractive && !km.isKeyguardLocked) {
+                    // DO NOT launch if we are in an emergency app OR if the package is empty
+                    if (blockType != null && topPkg.isNotBlank() && !LockManager.isEmergencyApp(topPkg) && !LockManager.isBootGraceActive() && pm.isInteractive && !km.isKeyguardLocked) {
                         val i = Intent(applicationContext, LockdownActivity::class.java)
                         i.putExtra("BLOCK_TYPE", blockType)
                         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_NO_ANIMATION)
