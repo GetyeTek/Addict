10c733637e059ce10acb2d6e521d6d3f250cfe70
GetyeTek <getyeteklu2@gmail.com>
2026-01-25 15:44:10 +0300

    Conduit: 1 ops

--- app/src/main/java/com/example/dnsguard/LockdownActivity.kt
+++ app/src/main/java/com/example/dnsguard/LockdownActivity.kt
@@ -56,8 +56,6 @@ class LockdownActivity : ComponentActivity() {
                         colors = listOf(uiConfig.color.copy(alpha = 0.4f), Color.Black),
                         radius = 2500f
                     )))
-                    // Glowing Border for entire screen
-                    Box(modifier = Modifier.fillMaxSize().padding(8.dp).border(4.dp, uiConfig.color, RoundedCornerShape(16.dp)))
                     
                     if (showPassDialog) {
                         MaintenanceDialog(onDismiss = { showPassDialog = false }) {
