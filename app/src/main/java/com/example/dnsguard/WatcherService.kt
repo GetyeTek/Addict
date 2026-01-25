501b5d23ac0803d40ce668862bc1071cb8aee8f6
GetyeTek <getyeteklu2@gmail.com>
2026-01-25 10:10:33 +0300

    Conduit: 1 ops

--- app/src/main/java/com/example/dnsguard/WatcherService.kt
+++ app/src/main/java/com/example/dnsguard/WatcherService.kt
@@ -57,17 +57,19 @@ class WatcherService : Service() {
                         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_NO_ANIMATION)
                         startActivity(i)
                     } 
-                    else if (!hasNotifs && !LockManager.isBootGraceActive()) {
+                    else if (!hasNotifs && !LockManager.isBootGraceActive() && !LockManager.isPermissionFixActive(applicationContext)) {
+                        LockManager.startPermissionFixSession(applicationContext)
                         val i = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
                         i.putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
                         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_NO_ANIMATION)
                         try { startActivity(i) } catch (e: Exception) {}
                     }
-                    else if (!hasAcc && !LockManager.isBootGraceActive()) {
-                        val yankIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-                        yankIntent.data = Uri.parse("package:$packageName")
-                        yankIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_NO_ANIMATION)
-                        try { startActivity(yankIntent) } catch (e: Exception) {}
+                    else if (!hasOverlay && !LockManager.isBootGraceActive() && !LockManager.isPermissionFixActive(applicationContext)) {
+                        LockManager.startPermissionFixSession(applicationContext)
+                        val i = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+                        i.data = Uri.parse("package:$packageName")
+                        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                        try { startActivity(i) } catch (e: Exception) {}
                     }
                 }
                 
