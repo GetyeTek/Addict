5c5557906f90aa9943dae5f874fd756e43e52d46
GetyeTek <getyeteklu2@gmail.com>
2026-01-25 14:19:26 +0300

    Conduit: 1 ops

--- app/src/main/java/com/example/dnsguard/LockdownActivity.kt
+++ app/src/main/java/com/example/dnsguard/LockdownActivity.kt
@@ -11,6 +11,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
